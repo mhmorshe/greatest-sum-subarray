7af8cb293af59dbbb7503615a273c53c4c7ad7f8
Mahir Morshed <Mahir Morshed>
2017-04-15 14:58:33 -0700

    First commit

---
+++ main.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class main here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class main
+{
+    public static void main(String args[])
+    {
+       System.out.println("This is a test");
+    }
+
+}
